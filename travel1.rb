ff5b0250eaeddda441de0618a5e67f9633dcc093
RY0GA <ryoa.isota@gmail.com>
2021-02-13 18:44:28 +0900

    配列のキー名、ヒアドキュメントのインデント変更、予約人数の変数名を変更

--- travel1.rb
+++ travel1.rb
@@ -1,15 +1,15 @@
 plans = [
-  {name:"沖縄旅行", price:10000},
-  {name:"北海道旅行", price:20000},
-  {name:"九州旅行", price:15000}
+  {place:"沖縄旅行", price:10000},
+  {place:"北海道旅行", price:20000},
+  {place:"九州旅行", price:15000}
 ]
 
 puts <<~TEXT
 旅行プランを選択して下さい。
 
-1. 沖縄旅行（10000円）
-2. 北海道旅行（20000円）
-3. 九州旅行（15000円）
+    1. 沖縄旅行（10000円）
+    2. 北海道旅行（20000円）
+    3. 九州旅行（15000円）
 
 TEXT
 
@@ -25,29 +25,29 @@ end
 
 #選択されたプラン名を表示する処理
 chosen_plan = plans[select_plan_number - 1]
-puts "#{chosen_plan[:name]}ですね。"
+puts "#{chosen_plan[:place]}ですね。"
 puts "何名で予約されますか？"
 
 #入力された数が不正な値か判断する処理
 while true
   print "人数を入力>"
   #予約人数を入力する処理
-  num_of_reservations = gets.to_i
-  break if (num_of_reservations >= 1)
+  num_of_people = gets.to_i
+  break if (num_of_people >= 1)
     puts "1以上の数を入力してください"
 end
 
 #入力された人数を表示
-puts "#{num_of_reservations}名ですね。"
+puts "#{num_of_people}名ですね。"
 
 #合計金額を計算する処理
-total_price = chosen_plan[:price] * num_of_reservations
+total_price = chosen_plan[:price] * num_of_people
 #割引料金を計算する処理
 discount_total_price = (total_price * 0.9).floor
 
 #割引料金が適応されるか判断する処理
-if num_of_reservations >= 5
-  puts "#{num_of_reservations}名以上ですので10％％割引になります"
+if num_of_people >= 5
+  puts "#{num_of_people}名以上ですので10％％割引になります"
   puts "合計金額は#{discount_total_price}円になります。"
 else
   puts "合計金額は#{total_price}円になります。"
